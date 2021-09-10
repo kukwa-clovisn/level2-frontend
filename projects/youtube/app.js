086637eab26102c47129931489cfb7c0aaa15462
kukwa clovis <kukwaclovisngong3@gmail.com>
2021-09-10 16:54:51 +0100

    commiting the joint youtube application

---
+++ projects/youtube/app.js
@@ -0,0 +1,9 @@
+let menuIcon = document.querySelector(".menu-icon");
+let sidebar = document.querySelector(".sidebar");
+let container = document.querySelector(".container");
+
+
+menuIcon.addEventListener("click", () =>{
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+});
